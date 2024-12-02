aed8975b834080ee0aa4e28a9196b1b97a686a95
WailBoukhari1 <boukahri.wail002@gmail.com>
2024-12-02 11:03:49 +0100

    AP-13 category entity is done

---
+++ src/main/java/com/youcode/product_manage/dto/request/CategoryRequest.java
@@ -0,0 +1,12 @@
+package com.youcode.product_manage.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class CategoryRequest {
+    @NotBlank(message = "Category name is required")
+    private String name;
+    
+    private String description;
+} 
--- src/main/java/com/youcode/product_manage/mapper/CategoryMapper.java
+++ src/main/java/com/youcode/product_manage/mapper/CategoryMapper.java
@@ -1,4 +1,19 @@
 package com.youcode.product_manage.mapper;
 
-public class CategoryMapper {
+import com.youcode.product_manage.dto.request.CategoryRequest;
+import com.youcode.product_manage.dto.response.CategoryResponse;
+import com.youcode.product_manage.entity.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    
+    Category toEntity(CategoryRequest request);
+    
+    @Mapping(target = "productCount", expression = "java(category.getProducts().size())")
+    CategoryResponse toResponse(Category category);
+    
+    void updateEntityFromRequest(CategoryRequest request, @MappingTarget Category category);
 }
